f9bbf9c625e78252ab4bb8105dfd5eb3ac4d52a7
Mario Pereyra <pereyramario93@gmail.com>
2023-08-04 12:40:31 -0300

    Se hicieron modificaciones en 2 pestañas. Se agregó una página de contacto, este commit es puramente para testear el formulario. También se han modificado imágenes, y se han agregado placeholders para las mismas

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nombre = $_POST["nombre"];
+    $email = $_POST["email"];
+    $mensaje = $_POST["mensaje"];
+    $asunto = $_POST["subject"];
+
+    // Aquí puedes realizar el envío del correo utilizando la función mail() de PHP
+    // o utilizando una librería de correo como PHPMailer o Swift Mailer
+    // En este ejemplo utilizamos la función mail(), pero ten en cuenta que esta
+    // función puede no funcionar correctamente en todos los servidores.
+
+    $destinatario = "zeerooh1993@gmail.com";
+    $cabeceras = "From: $nombre <$email>\r\n";
+    $cabeceras .= "Reply-To: $email\r\n";
+    $cabeceras .= "Content-Type: text/plain; charset=utf-8\r\n";
+
+    mail($destinatario, $asunto, $mensaje, $cabeceras);
+
+    // Redirigir al usuario a una página de "gracias" o mostrar un mensaje de éxito
+    header("Location: /gracias.html");
+    exit;
+}
+?>
